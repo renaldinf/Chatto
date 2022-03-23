9e2563d49175c24b066c37114dadad8728eff75f
Renaldi <renaldinajwanfauzan@gmail.com>
2022-03-23 08:28:13 +0700

    Initial Commit

---
+++ Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by KTMacBook on 23/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
